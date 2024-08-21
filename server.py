0bdedb96c080943b11f5b6b84add2c0a55dfe3c0
Thalyson Rian <Trungr@hotmail.com>
2024-08-21 14:57:35 -0300

    Update server.py
    Mudando de servidor ECO para servidor com MULTIPLAS conexões, refiz o código para o servidor suportar várias conexões. Por enquanto, criei a função para tratar o cliente, informar o tamanho dos dados que irei receber e opções para fechar a conexão "sair".

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 import socket
+import threading
+
 
 host = '192.168.2.6'
 port = 8086
+SERVER = socket.gethostbyname(socket.gethostname())
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((host, port))
-    server.listen()
-    conexao, endereco = server.accept()
-    with conexao:
-        print(f"Usuário Conectado, endereço:{endereco}")
-        while True:
-            data= conexao.recv(1024)
-            if not data:
-                break
-            conexao.sendall(data)
+server =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+
+
+def tratar_cliente(conec, addr):
+    print(f"[Nova conexão] {addr} conectado...")
+    conectado = True
+    while conectado:
+        msg_length = conec.recv(1024).decode("UTF-8")
+        if msg_length:
+            msg_length = int (msg_length)
+            msg= conec.recv(msg_length).decode("UTF-8")
+            if msg == "sair":
+                conectado = False
+            print(f"[{addr}] {msg}")
+        conec.send("Mensagem recebida".encode("UTF-8"))
+    conec.close()
+
