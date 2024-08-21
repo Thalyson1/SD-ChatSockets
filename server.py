f313d04382289c7d3911c8c45967e964375abb10
Thalyson Rian <Trungr@hotmail.com>
2024-08-21 13:33:51 -0300

    Update server.py
    Criando um servidor simples, o programa irá esperar dados do cliente, enquanto o cliente mandar o programa irá rodar, ao finalizar o programa irá dar um break e enviar os dados que o cliente informou.

--- server.py
+++ server.py
@@ -5,3 +5,12 @@ port = 8086
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.bind(host, port)
+    server.listen()
+    conexao, endereco = server.accept()
+    with conexao:
+        print(f"Conectado {endereco}")
+        while True:
+            data= conexao.recv(1024)
+            if not data:
+                break
+            conexao.sendall(data)
