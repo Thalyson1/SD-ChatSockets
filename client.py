57f56ae4a9bf947470c23fae71922e6425c31495
Thalyson Rian <Trungr@hotmail.com>
2024-08-29 18:54:52 -0300

    Atualizando código

--- client.py
+++ client.py
@@ -1,51 +1,122 @@
 import socket
+
 import threading
 
-host = '192.168.2.6'
+import re
+
+
+
+
+host = '25.51.113.214'
+
 port= 8086
 
 
+
+
+
+
 client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 client.connect((host, port))
 
+
+
+
 def enviar_msg():
-    #client vai enviar o nome
-    try:
-        nomeUser = input("Digite seu nick: ")
-        comando_nick = f"{nomeUser}"
-        print(f"Enviando comando: {comando_nick}")
-        client.send(comando_nick.encode('utf-8'))
+
+   #client vai enviar o nome
+
+   try:
+
+       nomeUser = input("Digite seu nick: ")
+
+       comando_nick = f"!nick {nomeUser}"
+
+       print(f"Enviando comando: {comando_nick}")
+
+       client.send(comando_nick.encode('utf-8'))
 
 
-        #função threading para ficar recebendo dados do servidor sempre que tiver att.
-        def receber_do_server():
-            while True:
-                try:
-                    #pega os dados
-                    response = client.recv(1024).decode('utf-8')
-                    
-                    if not response:
-                        print("Conexão fechada")
-                        break
-                    #mostra a(s) mensagem(s) do(s) usuário(s)
-                    print(f"\n{response}")
-                except:
-                    print("error")
-                    break
-        #aqui mostra os usuários que estão conectados       
-        response_Users = client.recv(1024).decode('utf-8')
-        print(f"Usuários conectados: {response_Users}")
-        threading.Thread(target=receber_do_server, daemon=True).start()
-
-        #receber a msg e tratamento para sair da aplicação
-        while True:
-            msg = input("Digite uma mensagem: \n")
-            if msg.lower() == 'sair':
-                break
-            client.send(msg.encode('utf-8'))
 
 
-    finally:
-        client.close()
-        
+
+
+       #função threading para ficar recebendo dados do servidor sempre que tiver att.
+
+       def receber_do_server():
+
+           while True:
+
+               try:
+
+                   #pega os dados
+
+                   response = client.recv(1024).decode('utf-8')
+
+                   
+
+                   if not response:
+
+                       print("Conexão fechada")
+
+                       break
+
+                   #mostra a(s) mensagem(s) do(s) usuário(s)
+
+                   print(f"\n{response}")
+
+               except:
+
+                   print("error")
+
+                   break
+
+       #aqui mostra os usuários que estão conectados       
+
+       response_Users = client.recv(1024).decode('utf-8')
+
+       print(f"Usuários conectados: {response_Users}")
+
+       threading.Thread(target=receber_do_server, daemon=True).start()
+
+
+
+
+       #receber a msg e tratamento para sair da aplicação
+
+       while True:
+
+           msg = input("Digite uma mensagem: \n")
+
+           if msg.lower() == 'sair':
+
+               break
+
+
+           if re.match(r"^!poke", msg):
+
+               client.send(msg.encode())
+
+               
+           elif re.match(r"^!changenickname", msg):
+
+               client.send(msg.encode())
+ 
+
+           else:
+
+               client.send(f"!sendmsg {msg}".encode('utf-8'))
+
+
+
+
+
+
+   finally:
+
+       client.close()
+
+       
+
 enviar_msg()
--- server.py
+++ server.py
@@ -1,109 +1,242 @@
+SERVER:
+
+
+
+
+
+
+
 import socket
+
 import threading
 
-host = '192.168.2.6'
+
+
+
+host = '25.51.113.214'
+
 port = 8086
+
 SERVER = socket.gethostbyname(socket.gethostname())
 
+
+
+
 clientes = {}
+
 lock = threading.Lock()
 
+
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind((host, port))
 
+
+
+
 def tratar_cliente(conec, addr):
-    with conec:
-        print(f"[Nova conexão] {addr} conectado...")
 
-        try:
-            # Recebe o nome do cliente
-            nomeUser = conec.recv(1024).decode('utf-8')
+   with conec:
 
-            #tratamento da tag
-            if not nomeUser.startswith("!nick"):
-                print("Error, você deve enviar seu nickname primeiro '!nick nome'. \nServidor fechado...")
-                conec.close()
-            else:
-                nomeUser = nomeUser[len("!nick"):].strip()
-
-                #Crie uma lista para inserir todos os users e dps mostrar eles para os usuários novos.
-                with lock:
-                    clientes[conec] = nomeUser
-                    print(f"Nome do usuário '{nomeUser}' registrado!")
-                    lista_usuarios = ' '.join(clientes.values())
-                    response = f"!users {len(clientes)} {lista_usuarios}"
-                    conec.sendall(response.encode('utf-8'))
-
-            # O servidor vai receber as mensagens do usuário
-            while True:
-                try:
-                    #devo iniciar +- aqui o tratamento de mensagens com a tag !send
-                    msg = conec.recv(1024).decode("utf-8")
-
-                    if msg.startswith("!sendmsg"):
-
-                        msg = msg[len("!sendmsg"):].strip()
-                        print(f"!msg {nomeUser} : {msg}")
-                        chat_user = f"{nomeUser}: {msg}"
-                        broadcast(chat_user, conec)
+       print(f"[Nova conexão] {addr} conectado...")
 
 
-                    elif msg.startswith("!changenickname"):
-                        nomeUserstate = nomeUser
-                        newNomeUser = msg[len("!changenickname"):].strip()
 
-                        if newNomeUser:
-                            nomeUser= newNomeUser
-                            clientes[conec] = nomeUser
-                            broadcast(f"{nomeUserstate} trocou o nome para {nomeUser}\n")
-                        else:
-                            conec.send("Error: falha ao atualizar nome")
-                    
-                    elif msg.startswith("!poke"):
-                        pokeUser = msg.split(' ')[1].strip()
-                        if pokeUser in clientes.values():
-                            broadcast(f"!poke {nomeUser} cutucou {pokeUser}\n")
-                        else:
-                            conec.send("Error: Usuário não encontrado")
 
-                    else:
-                        broadcast(f"Error, {nomeUser} você deve informar a tag '!sendmsg' para enviar uma mensagem, '!changenickname' para trocar o nome, ou '!poke' para cutucar um usuário, tente novamente\n")
-                    
-                except:
-                    break
+       try:
 
-        #caso naão dê certo, fechar conexão
-        finally:
-            with lock:
-                if conec in clientes:
-                    del clientes[conec]
-            print(f"Conexão {addr} fechada.")
-            conec.close()
+           # Recebe o nome do cliente
+
+           nomeUser = conec.recv(1024).decode('utf-8')
+
+
+
+
+           #tratamento da tag
+
+           if not nomeUser.startswith("!nick"):
+
+               print("Error, você deve enviar seu nickname primeiro '!nick nome'. \nServidor fechado...")
+
+               conec.close()
+
+           else:
+
+               nomeUser = nomeUser[len("!nick"):].strip()
+
+
+
+
+               #Crie uma lista para inserir todos os users e dps mostrar eles para os usuários novos.
+
+               with lock:
+
+                   clientes[conec] = nomeUser
+
+                   print(f"Nome do usuário '{nomeUser}' registrado!")
+
+                   lista_usuarios = ' '.join(clientes.values())
+
+                   response = f"!users {len(clientes)} {lista_usuarios}"
+
+                   conec.sendall(response.encode('utf-8'))
+
+
+
+
+           # O servidor vai receber as mensagens do usuário
+
+           while True:
+
+               try:
+
+                   #devo iniciar +- aqui o tratamento de mensagens com a tag !send
+
+                   msg = conec.recv(1024).decode("utf-8")
+
+
+
+
+                   if msg.startswith("!sendmsg"):
+
+
+
+
+                       msg = msg[len("!sendmsg"):].strip()
+
+                       print(f"!msg {nomeUser} : {msg}")
+
+                       chat_user = f"{nomeUser}: {msg}"
+
+                       broadcast(chat_user, conec)
+
+
+
+
+
+
+                   elif msg.startswith("!changenickname"):
+
+                       nomeUserstate = nomeUser
+
+                       newNomeUser = msg[len("!changenickname"):].strip()
+
+
+
+
+                       if newNomeUser:
+
+                           nomeUser= newNomeUser
+
+                           clientes[conec] = nomeUser
+
+                           broadcast(f"{nomeUserstate} trocou o nome para {nomeUser}\n")
+
+                       else:
+
+                           conec.send("Error: falha ao atualizar nome")
+
+                   
+
+                   elif msg.startswith("!poke"):
+
+                       pokeUser = msg.split(' ')[1].strip()
+
+                       if pokeUser in clientes.values():
+
+                           broadcast(f"!poke {nomeUser} cutucou {pokeUser}\n")
+
+                       else:
+
+                           conec.send("Error: Usuário não encontrado")
+
+
+
+
+                   else:
+
+                       broadcast(f"Error, {nomeUser} você deve informar a tag '!sendmsg' para enviar uma mensagem, '!changenickname' para trocar o nome, ou '!poke' para cutucar um usuário, tente novamente\n")
+
+                   
+
+               except:
+
+                   break
+
+
+
+
+       #caso naão dê certo, fechar conexão
+
+       finally:
+
+           with lock:
+
+               if conec in clientes:
+
+                   del clientes[conec]
+
+           print(f"Conexão {addr} fechada.")
+
+           conec.close()
+
+
+
+
+
+
+
 
 
 
 
 
 def broadcast(message, exclude_conn=None):
-    #Envia uma mensagem para todos os clientes conectados, exceto o próprio user.
-    with lock:
-        for conn in clientes:
-            if conn != exclude_conn:
-                try:
-                    conn.sendall(message.encode('utf-8'))
-                except:
-                    conn.close()
-                    if conn in clientes:
-                        del clientes[conn]
+
+   #Envia uma mensagem para todos os clientes conectados, exceto o próprio user.
+
+   with lock:
+
+       for conn in clientes:
+
+           if conn != exclude_conn:
+
+               try:
+
+                   conn.sendall(message.encode('utf-8'))
+
+               except:
+
+                   conn.close()
+
+                   if conn in clientes:
+
+                       del clientes[conn]
+
+
+
+
 
 
 def iniciar():
-    server.listen()
-    print(f"[Rodando...] Servidor está escutando em {SERVER}:{port}")
-    while True:
-        conec, addr = server.accept()
-        thread = threading.Thread(target=tratar_cliente, args=(conec, addr))
-        thread.start()
-        print(f"[Conexões Ativas] {threading.activeCount() - 1}")
+
+   server.listen()
+
+   print(f"[Rodando...] Servidor está escutando em {SERVER}:{port}")
+
+   while True:
+
+       conec, addr = server.accept()
+
+       thread = threading.Thread(target=tratar_cliente, args=(conec, addr))
+
+       thread.start()
+
+       print(f"[Conexões Ativas] {threading.activeCount() - 1}")
+
+
+
 
 iniciar()
